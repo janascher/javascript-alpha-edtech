fb07d18032dde9252522518ab3a5d3795dd0a0b3
Janaína Scher <janainascher@protonmail.com>
2022-06-09 22:22:37 -0300

    chore: add files

---
+++ aula06/js/page1.js
@@ -0,0 +1,32 @@
+const button = document.querySelector("#button");
+const h1 = document.querySelector("#message");
+const bombImage = document.querySelector("#image");
+const message = document.querySelector("#bomb");
+const audioExplosion = new Audio("./assets/sound/big_explosion_cut_off.ogg");
+
+let timer = null;
+
+button.addEventListener("click", bomb);
+
+function bomb() {
+    h1.innerHTML = "Rápido! Desarme a bomba em 10s!"
+    bombImage.src = "./assets/image/bomb.png";
+    bombImage.addEventListener("click", bombOff);
+    timer = setTimeout(armedBomb, 10000);
+}
+
+function armedBomb() {
+    button.innerHTML = "Tente de novo";
+    h1.innerHTML = "Ah, nãããão!!"
+    bombImage.src = "./assets/image/armed-bomb.png";
+    bombImage.removeEventListener("click", bombOff);
+    audioExplosion.volume = 0.4;
+    audioExplosion.play();
+}
+
+function bombOff() {
+    button.innerHTML = "Tente de novo";
+    h1.innerHTML = "Uuuuufa, foi por pouco..."
+    bombImage.src = "./assets/image/bomb-off.png";
+    clearTimeout(timer);
+}
