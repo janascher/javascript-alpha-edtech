9fa1a02a29fb81a3b421990ac28f38cc401105bb
Janaína Scher <janainascher@protonmail.com>
2022-07-06 13:08:25 -0300

    chore: add files

---
+++ aula14/pages/brigadeiros.js
@@ -0,0 +1,16 @@
+// import { goMainPage } from "./main.js";
+
+// export function goBrigadeiros() {
+//     const app = document.querySelector("#app");
+//     app.innerHTML = brigadeiros();
+
+//     const button = document.querySelectorAll(".button");
+//     button[0].addEventListener("click", goMainPage);
+// };
+
+export function brigadeiros() {
+    return `
+        <h2>Brigadeiros</h2>
+        <button id="main" class="button" type="button">Página inicial</button>
+    `
+};
