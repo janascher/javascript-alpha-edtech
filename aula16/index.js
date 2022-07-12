86be4c1c46ff427637b8bebf94c0a6f1aa71117e
Janaína Scher <janainascher@protonmail.com>
2022-07-12 09:16:44 -0300

    chore: add files

---
+++ aula16/index.js
@@ -0,0 +1,8 @@
+import { mainPage } from "./pages/main.js";
+// import { consultCep } from "./pages/consultCep.js";
+// import { showMap } from "./pages/showMap.js";
+
+const main = document.querySelector("#main");
+
+main.innerHTML = mainPage();
+
